bbdb130effda522a1c1d88d8566d9b56e22a6489
tconrad26 <tconrad@arcaneideas.com>
2026-01-18 19:33:28 -0500

    fixed signal flow in rtlsdr_iq_capture.  The old flow did not fix the DC line issue, the new one does.

--- gnuradio/rtlsdr_iq_capture.py
+++ gnuradio/rtlsdr_iq_capture.py
@@ -113,7 +113,7 @@ class rtlsdr_iq_capture(gr.top_block, Qt.QWidget):
         self._soapy_rtlsdr_source_0_setting_keys = [a.key for a in self.soapy_rtlsdr_source_0.get_setting_info()]
 
         self.soapy_rtlsdr_source_0.set_sample_rate(0, samp_rate)
-        self.soapy_rtlsdr_source_0.set_frequency(0, freq)
+        self.soapy_rtlsdr_source_0.set_frequency(0, (freq-if_freq))
         self.soapy_rtlsdr_source_0.set_frequency_correction(0, 0)
         self.set_soapy_rtlsdr_source_0_bias(bool(True))
         self._soapy_rtlsdr_source_0_gain_value = 10
@@ -213,8 +213,6 @@ class rtlsdr_iq_capture(gr.top_block, Qt.QWidget):
             blocks.FORMAT_PCM_32,
             False
             )
-        self.blocks_multiply_xx_0_0 = blocks.multiply_vcc(1)
-        self.blocks_multiply_xx_0_0.set_block_alias("mix_down")
         self.blocks_multiply_xx_0 = blocks.multiply_vcc(1)
         self.blocks_float_to_complex_0 = blocks.float_to_complex(1)
         self.blocks_complex_to_float_0 = blocks.complex_to_float(1)
@@ -228,8 +226,6 @@ class rtlsdr_iq_capture(gr.top_block, Qt.QWidget):
                 transition,
                 window.WIN_HAMMING,
                 6.76))
-        self.analog_sig_source_x_0_0 = analog.sig_source_c(samp_rate, analog.GR_COS_WAVE, (-if_freq), 1, 0, 0)
-        self.analog_sig_source_x_0_0.set_block_alias("tone_down")
         self.analog_sig_source_x_0 = analog.sig_source_c(samp_rate, analog.GR_COS_WAVE, if_freq, 1, 0, 0)
 
 
@@ -237,18 +233,16 @@ class rtlsdr_iq_capture(gr.top_block, Qt.QWidget):
         # Connections
         ##################################################
         self.connect((self.analog_sig_source_x_0, 0), (self.blocks_multiply_xx_0, 1))
-        self.connect((self.analog_sig_source_x_0_0, 0), (self.blocks_multiply_xx_0_0, 1))
-        self.connect((self.band_pass_filter_0, 0), (self.blocks_multiply_xx_0_0, 0))
+        self.connect((self.band_pass_filter_0, 0), (self.blocks_multiply_xx_0, 0))
         self.connect((self.blocks_complex_to_float_0, 0), (self.blocks_float_to_complex_0, 0))
         self.connect((self.blocks_complex_to_float_0, 1), (self.blocks_float_to_complex_0, 1))
         self.connect((self.blocks_complex_to_float_0, 1), (self.blocks_wavfile_sink_0, 1))
         self.connect((self.blocks_complex_to_float_0, 0), (self.blocks_wavfile_sink_0, 0))
         self.connect((self.blocks_float_to_complex_0, 0), (self.qtgui_freq_sink_x_0, 0))
         self.connect((self.blocks_float_to_complex_0, 0), (self.qtgui_waterfall_sink_x_0, 0))
-        self.connect((self.blocks_multiply_xx_0, 0), (self.band_pass_filter_0, 0))
-        self.connect((self.blocks_multiply_xx_0_0, 0), (self.low_pass_filter_0, 0))
+        self.connect((self.blocks_multiply_xx_0, 0), (self.low_pass_filter_0, 0))
         self.connect((self.low_pass_filter_0, 0), (self.blocks_complex_to_float_0, 0))
-        self.connect((self.soapy_rtlsdr_source_0, 0), (self.blocks_multiply_xx_0, 0))
+        self.connect((self.soapy_rtlsdr_source_0, 0), (self.band_pass_filter_0, 0))
 
 
     def closeEvent(self, event):
@@ -264,7 +258,7 @@ class rtlsdr_iq_capture(gr.top_block, Qt.QWidget):
 
     def set_freq(self, freq):
         self.freq = freq
-        self.soapy_rtlsdr_source_0.set_frequency(0, self.freq)
+        self.soapy_rtlsdr_source_0.set_frequency(0, (self.freq-self.if_freq))
 
     def get_outfile(self):
         return self.outfile
@@ -301,7 +295,6 @@ class rtlsdr_iq_capture(gr.top_block, Qt.QWidget):
     def set_samp_rate(self, samp_rate):
         self.samp_rate = samp_rate
         self.analog_sig_source_x_0.set_sampling_freq(self.samp_rate)
-        self.analog_sig_source_x_0_0.set_sampling_freq(self.samp_rate)
         self.band_pass_filter_0.set_taps(firdes.band_pass(1, self.samp_rate, (self.if_freq - self.chan_bw/2), (self.if_freq + self.chan_bw/2), self.transition, window.WIN_HAMMING, 6.76))
         self.low_pass_filter_0.set_taps(firdes.low_pass(1, self.samp_rate, (self.chan_bw/2), self.transition, window.WIN_HAMMING, 6.76))
         self.qtgui_freq_sink_x_0.set_frequency_range(0, self.samp_rate)
@@ -314,8 +307,8 @@ class rtlsdr_iq_capture(gr.top_block, Qt.QWidget):
     def set_if_freq(self, if_freq):
         self.if_freq = if_freq
         self.analog_sig_source_x_0.set_frequency(self.if_freq)
-        self.analog_sig_source_x_0_0.set_frequency((-self.if_freq))
         self.band_pass_filter_0.set_taps(firdes.band_pass(1, self.samp_rate, (self.if_freq - self.chan_bw/2), (self.if_freq + self.chan_bw/2), self.transition, window.WIN_HAMMING, 6.76))
+        self.soapy_rtlsdr_source_0.set_frequency(0, (self.freq-self.if_freq))
 
     def get_decimation(self):
         return self.decimation
